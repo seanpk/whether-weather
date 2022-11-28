277c4c13f9bf04170c106e667300bb92c42f704b
Sean Kennedy <seanpk@outlook.com>
2022-11-28 16:45:02 -0500

    Clean up new flow tests and update fixtures

--- spec/system/new_flow_spec.rb
+++ spec/system/new_flow_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "NewFlow", type: :system do
-  fixtures :locations
+RSpec.describe "straightforward flow to add a location and get a forecast for it", type: :system do
+  fixtures :locations, :forecasts
   
   before do
     driven_by(:rack_test)
@@ -11,7 +11,8 @@ RSpec.describe "NewFlow", type: :system do
     name: "London",
     political_unit: "Ontario, Canada",
     lat: 42.98339,
-    long: -81.23304
+    long: -81.23304,
+    uuid: "e9da05c9-136b-594f-ba53-33eb3f2b334a"
   )}
   let(:loc_exists) { Location.new(
     name: "Swan River",
@@ -21,22 +22,13 @@ RSpec.describe "NewFlow", type: :system do
     uuid: "b320f656-e0ab-5c1b-ad70-a10aba517847"
   )}
 
-  it "can search for a location from the home page" do
+  it "can search a location from the home page" do
     visit "/"
 
     fill_in "query", :with => loc_new.name
     click_button "Search"
 
     expect(page).to have_text("Matching Locations for \"#{loc_new.name}\"")
-  end
-
-  it "shows a list of matching search results" do
-    visit "/locations/search?query=#{loc_new.name}"
-
-    all('tr').each do |tr|
-      expect(tr).to have_text(loc_new.name)
-      expect(tr).to have_button("Add to My List")
-    end
   end
 
   it "can add a location to my results" do
@@ -54,13 +46,20 @@ RSpec.describe "NewFlow", type: :system do
     expect(page).to have_link("Get Forecast")
   end
 
-  it "can get a new forecast" do
-    visit "/locations/#{loc_exists.get_uuid}"
-
-    click_link "Get Forecast"
+  it "can get a an exiting forecast" do
+    visit location_forecast_path(loc_exists)
 
     expect(page).to have_text("Forecast for #{loc_exists.display_name}")
     expect(page).to have_text("°C")
+    expect(page).to have_text("Last updated: #{forecasts(:srmb).updated_at}")
     expect(page).to have_text("Next auto update after")
+  end
+
+  it "can get a an new forecast" do
+    location = locations(:raleigh)
+    visit location_forecast_path(location)
+
+    expect(page).to have_text("Forecast for #{location.display_name}")
+    expect(page).to have_text("Last updated: #{Forecast.find_by_location_id(location.id).updated_at}")
   end
 end
